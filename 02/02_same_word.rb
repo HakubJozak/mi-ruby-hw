dce84fc909d5a24f65713419c364b68492e867d0
Tomas Barton <barton.tomas@gmail.com>
2014-10-01 10:57:28 +0200

    second exercise

---
+++ 02/02_same_word.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+# Find duplication of words in a given
+# file and return a Hash indexed by the number
+# of lines and containing an Array of
+# words that were repeated
+def same_word(file)
+  # @TODO write implementation
+end
