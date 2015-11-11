6ff82bd1f3d5205eaf7bc7a0dfcce8ed110362a0
Jakub Hozak <jakub.hozak@gmail.com>
2015-11-11 13:49:08 +0100

    + vowel finder

--- 04/vowel/vowel_finder.rb
+++ 04/vowel/vowel_finder.rb
@@ -1,6 +1,33 @@
 #!/usr/bin/env ruby
 
-# looks for vowels
-class VowelFinder
+require 'pry'
 
+module Summable
+  def sum
+    reduce(&:+)
+  end
+end
+
+
+class VowelFinder
+  include Enumerable
+  include Summable
+  
+  def initialize(text)
+    @text = text
+  end
+
+  def each
+    return enum_for(__method__) unless block_given?
+
+    @text.scan /([aeiyou])/i do |match|
+      yield match.first.downcase
+    end
+    
+  end
+end
+
+
+class Array
+  include Summable
 end
